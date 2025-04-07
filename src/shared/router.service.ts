f5bc10071399aec19889a2bf12bf572423c060c0
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2025-04-07 10:56:40 -0500

    chore: changed main page to avoid problems with netlify

--- src/shared/router.service.ts
+++ src/shared/router.service.ts
@@ -1,12 +1,10 @@
 import {createRouter, createWebHistory} from "vue-router";
 import CalculatorPage from "../pages/calculator.page.vue";
-import HomePage from "../pages/home.page.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: "/", component: HomePage},
-        {path: "/calculator", component: CalculatorPage}
+        {path: "/", component: CalculatorPage}
     ],
     sensitive: true,
     strict: false,
